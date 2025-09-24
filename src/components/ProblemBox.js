9e7eb377574b5d19bbcde913d290bee870ca6492
HUANGCHIHHUNG <unrequited1991@gmail.com>
2025-09-24 08:09:19 +0800

    Update ProblemBox.js

--- src/components/ProblemBox.js
+++ src/components/ProblemBox.js
@@ -72,7 +72,7 @@ export default function ProblemBox({
       setShowExplanation(false);
       setIsCompleted(false);
     }
-  }, [currentProblem?.id]);
+  }, [currentProblem]);
 
   if (!currentProblem) {
     return (
